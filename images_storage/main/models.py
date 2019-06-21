834b045a9a8db98a8801211834b31abd35a22dce
Andrii Marynets <andrii.marynets@gmail.com>
2019-06-21 20:57:58 +0300

    Add image model

--- images_storage/main/models.py
+++ images_storage/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    original_image = models.ImageField()
+    resize_image = models.ImageField(null=True, default=None)
+    meta = models.TextField(default=None, null=True)

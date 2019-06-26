1fe0337cfab0bb1289b475c8fa0b53b0859fe060
Andrii Marynets <andrii.marynets@gmail.com>
2019-06-26 20:44:21 +0300

    Add base image displaying

--- images_storage/images_storage/urls.py
+++ images_storage/images_storage/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from users import views as users_views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('register/', users_views.register, name='register')
 
+urlpatterns = [
+    path('', include('main.urls')),
+    path('admin/', admin.site.urls),
+    path('register/', users_views.register, name='register')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

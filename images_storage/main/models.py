5a0a8301839fcc9eb2fa14a9a91e99862e56561e
Andrii Marynets <andrii.marynets@gmail.com>
2019-06-26 20:44:21 +0300

    Add base image displaying

--- images_storage/main/models.py
+++ images_storage/main/models.py
@@ -7,3 +7,6 @@ class Image(models.Model):
     original_image = models.ImageField(upload_to='original')
     resize_image = models.ImageField(null=True, default=None, blank=True, upload_to='processed')
     meta = models.TextField(default=None, null=True, blank=True)
+
+    def __str__(self):
+        return 'Image'
--- images_storage/main/urls.py
+++ images_storage/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('images/', views.ImageListView.as_view(), name='images')
 ]

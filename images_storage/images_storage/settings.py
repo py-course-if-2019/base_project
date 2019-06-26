ffc611955b004f041ee1d13c522b95c716763d8f
Andrii Marynets <andrii.marynets@gmail.com>
2019-06-26 19:45:42 +0300

    Add media root

--- images_storage/images_storage/settings.py
+++ images_storage/images_storage/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_URL = '/media/'
--- images_storage/main/models.py
+++ images_storage/main/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 
 class Image(models.Model):
-    original_image = models.ImageField()
-    resize_image = models.ImageField(null=True, default=None)
-    meta = models.TextField(default=None, null=True)
+    original_image = models.ImageField(upload_to='original')
+    resize_image = models.ImageField(null=True, default=None, blank=True, upload_to='processed')
+    meta = models.TextField(default=None, null=True, blank=True)

991abf7feed08126d3b6e50ea7e88483a34aa3ae
Alice <44687825+ruschakartur2@users.noreply.github.com>
2019-06-28 04:08:31 +0300

    Add login form, auth from facebook, design images

--- images_storage/images_storage/urls.py
+++ images_storage/images_storage/urls.py
@@ -15,14 +15,21 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
 
 urlpatterns = [
-    path('', include('main.urls')),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
+    path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
-    path('register/', users_views.register, name='register')
+    path('register/', users_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

fecd5f11f5b3461f56936e89b2125c86462b6570
Yaroslav Ryzhan <slavik.ryzhan@gmail.com>
2019-06-30 12:33:01 +0300

    Add two version Pillow (work)

--- images_storage/main/processor.py
+++ images_storage/main/processor.py
@@ -1,29 +1,35 @@
 from PIL import Image as Im
-import os
 from .models import Image
-
+import io
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
 def processed(Image_id, scale = 50):
 
     try:
-        oll = Image.objects.all()
         original = Image.objects.get(id=Image_id)
         path_in = original.original_image
-        path_out = original.resize_image
-        original_temp_image = Im.open(path_in)
 
-        original_size = (original_temp_image.width, original_temp_image.height)
-        size = (int(original_size[0] * (scale / 100)),int(original_size[1] * (scale / 100)))
-        # saved, ext = os.path.splitext(path_out)
+        img_io = io.BytesIO()
+        original_image = Im.open(path_in)
 
-        new_size = original_temp_image.resize(size)
-        # new_size.save(saved + f"_new_size_{size[0]}_size{size[1]}" + ext)
-        # new_size.save()
-        # original.resize_image = saved + f'_new_size{size}' + ext
-        original.resize_image = new_size
-        original.save(original.resize_image)
-        original_temp_image.close()
+        original_size = (original_image.width, original_image.height)
+
+        size = (int(original_size[0] * (scale / 100)), int(original_size[1] * (scale / 100)))
+
+        saved, file_name = original.original_image.name.split('/')
+        name, ext = file_name.split('.')
+
+        new_size = original_image.resize(size)
+        new_size.save(img_io, format='JPEG')
+
+
+        image_file = InMemoryUploadedFile(img_io, None, f'{name}_{size[0]}_{size[1]}.{ext}', 'image/jpeg',
+                                          img_io.tell, None)
+        original.resize_image.save(f'{name}_{size[0]}_{size[1]}.{ext}', image_file)
+
+        original.save()
+
 
     except FileNotFoundError:
         print("File not found")

1ad05f0697d01a3a0a61cd288b8acae5dd63d479
Andrii Marynets <andrii.marynets@gmail.com>
2019-07-02 23:22:07 +0300

    Add PSQL DB

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Near manage.py
 ```celery -A images_storage worker -l info``` - to run worker
 ```celery purge -A images_storage``` - clean queue
+
+
+heroku ps:scale worker=1
+heroku ps:scale web=1
--- images_storage/images_storage/settings.py
+++ images_storage/images_storage/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,12 +106,17 @@ WSGI_APPLICATION = 'images_storage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('PROD', 'no') == 'yes':
+    DATABASES = {
+        'default': dj_database_url.config(os.environ['DATABASE_URL'])
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
